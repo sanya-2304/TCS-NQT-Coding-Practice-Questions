04c4d13c6c3c1009930e9faf1c00c1ee6f3336c9
Sanya Doda <126786063+sanya-2304@users.noreply.github.com>
2024-10-02 15:59:02 +0530

    lcm

--- lcm_of_two_nums.cpp
+++ lcm_of_two_nums.cpp
@@ -1,1 +1,19 @@
-    
+#include <iostream>
+using namespace std;
+int gcd(int a, int b){
+    if(b==0) return a;
+    return gcd(b, a%b);
+}
+int lcm(int a, int b){
+    return (a*b)/gcd(a,b);
+}
+int main(){
+    int num1, num2;
+    cout << "Enter two integers: ";
+    cin >> num1 >> num2;
+    int gcdd=gcd(num1, num2);
+    cout<<"gcd is: "<<gcdd<<endl;
+    int lcmm=lcm(num1, num2);
+    cout<<"lcm is: "<<lcmm;
+    return 0;       
+}
